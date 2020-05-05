e945c5cd827175fec212d6d016642e186b0c9582
rfro377 <rjfromont@gmail.com>
2020-05-06 11:35:19 +1200

    Testing FreqInt and first program for Riley
    Testing FreqInt and first program for Riley
    
    Expected print of values to screen

--- software/freeRTOS_A1/freertos_test.c
+++ software/freeRTOS_A1/freertos_test.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/*Freq Analyser include*/
+#include <unistd.h>
+#include "system.h"
+#include "sys/alt_irq.h"
+#include "io.h"
+#include "altera_avalon_pio_regs.h"
+
+
+
 /* Scheduler includes. */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -19,8 +28,20 @@ static void prvSecondRegTestTask(void *pvParameters);
 /*
  * Create the demo tasks then start the scheduler.
  */
+
+//Frequency Analyser
+
+void freq_relay(){
+#define SAMPLING_FREQ 16000.0
+	double temp = SAMPLING_FREQ/(double)IORD(FREQUENCY_ANALYSER_BASE, 0);
+	//temp contains Freq value
+	printf("%f Hz\n", temp);
+	//Send to Queue
+}
+
 int main(void)
 {
+	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
 	/* The RegTest tasks as described at the top of this file. */
 	xTaskCreate( prvFirstRegTestTask, "Rreg1", configMINIMAL_STACK_SIZE, mainREG_TEST_1_PARAMETER, mainREG_TEST_PRIORITY, NULL);
 	xTaskCreate( prvSecondRegTestTask, "Rreg2", configMINIMAL_STACK_SIZE, mainREG_TEST_2_PARAMETER, mainREG_TEST_PRIORITY, NULL);
@@ -36,7 +57,7 @@ static void prvFirstRegTestTask(void *pvParameters)
 {
 	while (1)
 	{
-		printf("Task 1\n");
+		//printf("Task 1\n");
 		vTaskDelay(1000);
 	}
 
@@ -45,7 +66,8 @@ static void prvSecondRegTestTask(void *pvParameters)
 {
 	while (1)
 	{
-		printf("Task 2\n");
+		//printf("Task 2\n");
 		vTaskDelay(1000);
 	}
 }
+
